5bd502919803d15165287beee2a7b8de7a3cd046
romeodombaj <romeodombaj@gmail.com>
2024-08-29 06:03:57 +0200

    POST REQUEST created

--- routes/users.js
+++ routes/users.js
@@ -14,24 +14,36 @@ function readDB() {
 
 // write data to db
 function writeDB(data) {
-  fs.writeFile(db, JSON.stringify(data, null, 2), "utf8");
+  console.log("HERE");
+  fs.writeFileSync(db, JSON.stringify(data, null, 2), "utf8");
+  console.log("HERE2");
 }
 
 // get all user data
 router.get("/", async (req, res) => {
+  console.log("GETTING USER DATA");
   const users = readDB();
-  res.code(200).json(users);
+  res.status(200).json(users);
 });
 
-// add new user/s
+// add new user
 router.post("/new", jsonParser, (req, res) => {
-  console.log("POST");
-  const user = req.body.values;
+  console.log("ADDING A NEW USER");
+
+  console.log(req.body);
+  const users = readDB();
+  const newUser = {
+    id: users.length ? users[users.length - 1].id + 1 : 1,
+    ...req.body.values,
+  };
+  users.push(newUser);
+  writeDB(users);
+  res.status(201).json(newUser);
 });
 
 // edit user
 router.put("/:id", jsonParser, (req, res) => {
-  console.log("PUT");
+  console.log("EDITING A USER");
   const id = req.body.key;
   const user = req.body.values;
 
@@ -41,7 +53,7 @@ router.put("/:id", jsonParser, (req, res) => {
 
 // delete user
 router.delete("/:id", (req, res) => {
-  console.log("DELETE");
+  console.log("DELETING USER");
   console.log(req.body);
 
   let users = readDB();
